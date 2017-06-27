666cd1ea2c4c5d072a79c9c57552919278032e3b
xiongzhd <986137289@qq.com>
2017-06-27 10:29:29 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/administrator/coolweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.administrator.coolweather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
